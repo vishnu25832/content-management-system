a02b36b53e22a729f3f92fe5b7b27788e83027a0
Vishnu Vardhan <vishnuvardhan25072003@gmail.com>
2026-07-25 07:22:36 +0530

    Debug API URL 2

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,12 +1,26 @@
 import axios from "axios";
 
-console.log("NEXT_PUBLIC_API_URL =", process.env.NEXT_PUBLIC_API_URL);
+const API_URL = (process.env.NEXT_PUBLIC_API_URL || "").replace(/\/+$/, "");
+
+console.log("NEXT_PUBLIC_API_URL =", API_URL);
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+  baseURL: `${API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
 });
 
 export default api;
